7724d803c043dada0109a712718767229831cf33
vinicioslop <vinicioslop7@hotmail.com>
2021-12-07 21:47:28 -0300

    Implementa código mexido na aula.

---
+++ variavel_var/variavel_var/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace variavel_var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var num = 15;
+            /*
+             * var num;
+             * num = 10;
+             * 
+             * resulta em erro porque a variavel não foi iniciada com um
+             * valor válido.
+             */
+        }
+    }
+}
