8e6a45c3c95de9e7abe933bf59f09196dab943d2
vinicioslop <vinicioslop7@hotmail.com>
2021-12-07 21:43:07 -0300

    Disponibiliza código feito em aula.

---
+++ comentarios/comentarios/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace comentarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prints Hello
+            // Console.WriteLine("Hello");
+            /* Some long
+             *  comment text
+             */
+            int x = 42;
+            Console.WriteLine(x);
+        }
+    }
+}
