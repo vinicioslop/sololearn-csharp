480cf67e58cdcca8887d4d1780c2ff18456b0905
vinicioslop <vinicioslop7@hotmail.com>
2021-12-07 21:50:51 -0300

    Implmenta código feito em aula

---
+++ cosntantes/cosntantes/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace constantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.14;
+            // PI = 8; - RESULTA EM ERRO
+        }
+    }
+}
