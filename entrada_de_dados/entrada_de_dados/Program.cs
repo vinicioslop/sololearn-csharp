c9d16d0ff39f169dce1c472dc8a855ca6bb00e7e
vinicioslop <vinicioslop7@hotmail.com>
2021-12-07 21:38:20 -0300

    Disponibiliza os códigos apresentados na aula.

---
+++ entrada_de_dados/entrada_de_dados/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace entrada_de_dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXEMPLOS
+
+            // string yourName;
+            // Console.WriteLine("Whats is your name?");
+            // yourName = Console.ReadLine();
+            // Console.WriteLine("Helo {0}", yourName);
+
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You are {0} years old", age);
+        }
+    }
+}
