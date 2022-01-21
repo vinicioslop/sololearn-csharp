b0674fabada27f35d4617b869989d7df4c208da9
vinicioslop <vinicioslop7@hotmail.com>
2022-01-21 18:22:43 -0300

    Finish the module 08 of the course

---
+++ Projetos/RobotBarman/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace RobotBarman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // You have a robot-barman and his goal is to neatly arrange
+            // drinks on the shelves of the bar. He is very smart and 
+            // takes as many drinks as are necessary to evenly distribute 
+            // them on the shelves, but he still has problems with division.
+
+            // The program installed in the robot takes the number of drinks
+            // and the number of the shelves as input.
+            // Complete the program to evenly distribute the drinks on shelves: 
+            // by dividing the number of drinks by the number of shelves and 
+            // outputting the result.
+            // The program must also, handle those two possible problems:
+            // 1. the divider (the number of shelves) should never be zero
+            // 2. both inputs should be integers.
+            // For the first exception, the program should output "At least 1 shelf" 
+            // and for the second, "Please insert an integer".
+
+            // Sample Input
+            // 6
+            // two
+
+            // Sample Output
+            // Please insert an integer
+
+            // Use DivideByZeroException for the first exception and FormatException 
+            // for the second one.
+            try
+            {
+                int drinks = Convert.ToInt32(Console.ReadLine());
+                int shelves = Convert.ToInt32(Console.ReadLine());
+
+                //your code goes here
+                double result = drinks / shelves;
+                Console.WriteLine(result);
+            }
+            /*
+             * 1. DivideByZeroException => "At least 1 shelf"
+             * 2. FormatException => "Please insert an integer"
+            */
+            catch (DivideByZeroException ze)
+            {
+                Console.WriteLine("At least 1 shelf");
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("Please insert an integer");
+            }
+        }
+    }
+}
---
+++ Quizes/QuizModulo08/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace QuizModulo08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Question01();
+            Question02();
+            Question03();
+            Question04();
+            Question05();
+            Question06();
+        }
+        static void Question01()
+        {
+            Console.WriteLine();
+
+            Console.WriteLine("QUESTION 01");
+            Console.WriteLine("Can you inherit from a struct?");
+            Console.WriteLine("R: No");
+        }
+        static void Question02()
+        {
+            Console.WriteLine();
+
+            Console.WriteLine("QUESTION 02");
+            Console.WriteLine("What is the output of this code?");
+            Console.WriteLine("enum Test { A, B, C=5, D };");
+            Console.WriteLine("static void Main(string[] args) {");
+            Console.WriteLine("int x = (int)Test.D;");
+            Console.WriteLine("Console.WriteLine(x);");
+            Console.WriteLine("R: 6");
+        }
+        static void Question03()
+        {
+            Console.WriteLine();
+
+            Console.WriteLine("QUESTION 03");
+            Console.WriteLine("int is a Struct");
+            Console.WriteLine("R: True");
+        }
+        static void Question04()
+        {
+            Console.WriteLine();
+
+            Console.WriteLine("QUESTION 04");
+            Console.WriteLine("Which of the following is the correct");
+            Console.WriteLine("way to define a variable of the struct");
+            Console.WriteLine("Person declared below?");
+            Console.WriteLine("struct Person {");
+            Console.WriteLine("     private string name;");
+            Console.WriteLine("     private int age; ");
+            Console.WriteLine("}");
+            Console.WriteLine("R: Person p;");
+        }
+        static void Question05()
+        {
+            Console.WriteLine();
+
+            Console.WriteLine("QUESTION 05");
+            Console.WriteLine("What is the value of x after this code?");
+            Console.WriteLine("int x = 0;");
+            Console.WriteLine("try {");
+            Console.WriteLine("     x /= x;");
+            Console.WriteLine("     x += 1;");
+            Console.WriteLine("}");
+            Console.WriteLine("catch (Exception e) {");
+            Console.WriteLine("     x += 3;");
+            Console.WriteLine("}");
+            Console.WriteLine("finally {");
+            Console.WriteLine("     x += 4;");
+            Console.WriteLine("}");
+            Console.WriteLine("R: 7");
+        }
+        static void Question06()
+        {
+            Console.WriteLine();
+
+            Console.WriteLine("QUESTION 06");
+            Console.WriteLine("Fill in the blanks to handle exceptions.");
+            Console.WriteLine("int x = 0;");
+            Console.WriteLine("try {");
+            Console.WriteLine("     x = Convert.ToInt32(\"AAA\")");
+            Console.WriteLine("}");
+            Console.WriteLine("catch (Exception e) {");
+            Console.WriteLine("     Console.WriteLine(\"Error\")");
+            Console.WriteLine("}");
+        }
+    }
+}
